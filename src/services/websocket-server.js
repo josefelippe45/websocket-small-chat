100987f7cda4e6bf9052f8eebdbb04f2b955c670
José Felippe Gonçalves Oliveira <jose.oliveira370@etec.sp.gov.br>
2020-09-12 13:17:49 -0300

    sending form data

--- src/services/websocket-server.js
+++ src/services/websocket-server.js
@@ -4,7 +4,12 @@ const port = process.env.PORT || 3000;
 const WSS = new WebSocketServer({port: port});
 //listen for conections. every time a client does the connection, for example: does a request
 // the callback is evoked.
+//ws is the callback of the data sent by the client file. here it is handled
 WSS.on('connection', (ws)=>{
+    //ws is our client. listening for a message. NOTE- the first parameter 'message' is build-in
+    ws.on('message', (message)=>{
+        //handle the message
+        console.log(message);
+    })
     console.log('You are connected');
-})
-console.log('works');
+});
